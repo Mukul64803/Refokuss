b27d6f4709db76f485d5cb28fe17e28b689ffd9c
Mukul64803 <msharma64803@gmail.com>
2024-09-09 01:48:28 +0530

    final Commit

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import Button from './Button'
+/* eslint-disable react/prop-types */
+import React from "react";
+import Button from "./Button";
 
-function Product({val,count,mover}) {
+function Product({ val,mover,count }) {
   return (
-    <div onMouseEnter={()=>{mover(count)}}  className='max-w-screen-xl h-[15rem] mx-auto mb-14'>
-        <div className=' flex items-center justify-between'>
-            <h1 className='text-5xl '>{val.title}</h1>
-            <div className='w-1/4 mx-10 py-5'>
-                <p className='mb-8 text-lg'>{val.description}</p>
-                <div className='flex items-center gap-5'>
-                    {val.live && <Button title={"Live Website"}/>}
-                    {val.case && <Button title={"Case Study"} />}
-                </div>
-            </div>
+    <div className="w-full py-20 h-[23rem] text-white">
+      <div onMouseEnter={()=>{mover(count)}} className="max-w-screen-xl mx-auto flex items-center justify-between">
+        <h1 className="text-5xl capitalize font-medium">{val.title}</h1>
+        <div className="dets w-1/4">
+          <p className="mb-10">{val.description}</p>
+          <div className="flex items-center gap-5">
+            {val.live && <Button title="Live Project" />}
+            {val.case && <Button title="Case Study" />}
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,50 +1,137 @@
-import React, { useState } from 'react'
-import Product from './Product'
-import { motion } from 'framer-motion'
+import React, { useState } from "react";
+import Product from "./Product";
+import { motion } from "framer-motion";
+import arqitel from "../assets/arqitel.mp4";
+import ttr from "../assets/ttr.mp4";
+import yir from "../assets/yir.mp4";
+import yahoo from "../assets/yahoo.mp4";
+import rainfall from "../assets/rainfall.mp4";
 
 function Products() {
-    let products_details = [
-        {title: "Arqitel", description: "With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.", live: true, case: false },
-        {title: "Cula", description: "We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.", live: true, case: false },
-        {title: "Layout Land", description: "An interactive learning game that can educate and entertain you on the basics of web layouts in Webflow.", live: true, case: false },
-        {title: "TTR", description: "We've created an interactive site using generative AI to allow users to engage with our thinking about Ai, industry trends and design.", live: true, case: false },
-        {title: "Maniv", description: "A global early-stage venture fund partnering with founders to advance cleaner, safer, and more sustainable movement of people and goods.", live: true, case: false },
-        {title: "Like Magic", description: "We designed and developed a magical gaming experience made in Webflow to promote the translation service and their sponsorship of the 2022 Webflow Conference.", live: true, case: true },
-        {title: "Silvr", description: "We teamed up with financing solutions provider Silvr to audit, refine and evolve their brand.", live: true, case: false },
-        {title: "Jungle", description: "We crafted a timeless visual system for Jungle Ventures, covering all aspects of web design, and empowered their marketing team to scale organically using Webflow.", live: true, case: true },
-        {title: "Yahoo!", description: "We enhanced the New York Fashion Week, by creating a fully digital AR fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models and an integrated web shop.", live: true, case: true },
-        {title: "YIR 2022", description: "Our second year was filled with great events, exciting projects, awards and amazing people - so we made another showcase to celebrate.", live: true, case: false },
-    ]
+  var products = [
+    {
+      title: "Arqitel",
+      description:
+        "With a continuous 3D animation, we showcase Arqitel approach and show how migration data translates into real estate.",
+      live: true,
+      case: false,
+    },
+    {
+      title: "TTR",
+      description:
+        "We've created an interactive site using generative AI to allow users to engage with our thinking about Ai, industry trends and design.",
+      live: true,
+      case: false,
+    },
+    {
+      title: "YIR 2022",
+      description:
+        "Our second year was filled with great events, exciting projects, awards and amazing people - so we made another showcase to celebrate.",
+      live: true,
+      case: false,
+    },
+    {
+      title: "Yahoo!",
+      description:
+        "We enhanced the New York Fashion Week, by creating a fully digital AR fashion experience for Yahoo and Maisie Wilen, featuring holographic 3D models and an integrated web shop.",
+      live: true,
+      case: true,
+    },
+    {
+      title: "Rainfall",
+      description:
+        "We crafted a website for Rainfall Ventures, developing prototypes and custom code that ultimately allows their team to update content regularly and with ease.",
+      live: true,
+      case: true,
+    },
+  ];
 
-    const [pos,setpos] = useState(0);
-
-    const mover = (val)=>{
-      setpos(val*18);
-    }
+  const [pos, setPos] = useState(0);
+  const mover = (val) => {
+    setPos(val * 23);
+  };
 
   return (
-    <div className='mt-36 relative'>
-        {products_details.map((elem,index)=>(
-            <Product val={elem} key={index} count={index} mover={mover} />
-        ))}
-        <div className='absolute top-0 w-full h-full pointer-events-none'>
-          <motion.div initial={{y: 0,x: "-45%"}} animate={{y: pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.6}} className='absolute w-[23rem] h-[18rem] bg-sky-200 left-[45%] rounded-md overflow-hidden'>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-200'>
-            
-            </motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-300'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-400'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-500'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-600'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-700'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-800'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-900'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-200'></motion.div>
-            <motion.div animate={{y: -pos + `rem`}} transition={{ease: [0.25, 1, 0.5, 1],duration: 0.5}} className='w-full h-full bg-green-300'></motion.div>
+    <div className="mt-32 relative">
+      {products.map((val, index) => (
+        <Product key={index} val={val} mover={mover} count={index} />
+      ))}
+      <div className="absolute top-0 w-full h-full pointer-events-none">
+        <motion.div
+          initial={{ y: pos, x: "-50%" }}
+          animate={{ y: pos + `rem` }}
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.6 }}
+          className="window absolute w-[32rem] h-[23rem] left-[44%] rounded-3xl overflow-hidden"
+        >
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+            className="w-full h-full"
+          >
+            <video
+              className="absolute object-cover rounded-3xl"
+              autoPlay
+              muted
+              loop
+              src={arqitel}
+            ></video>
           </motion.div>
-        </div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+            className="w-full h-full"
+          >
+            <video
+              className="absolute object-cover rounded-3xl"
+              autoPlay
+              muted
+              loop
+              src={ttr}
+            ></video>
+          </motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+            className="w-full h-full"
+          >
+            <video
+              className="absolute object-cover rounded-3xl"
+              autoPlay
+              muted
+              loop
+              src={yir}
+            ></video>
+          </motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+            className="w-full h-full"
+          >
+            <video
+              className="absolute object-cover rounded-3xl"
+              autoPlay
+              muted
+              loop
+              src={yahoo}
+            ></video>
+          </motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+            className="w-full h-full"
+          >
+            <video
+              className="absolute object-cover rounded-3xl"
+              autoPlay
+              muted
+              loop
+              src={rainfall}
+            ></video>
+          </motion.div>
+        </motion.div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
